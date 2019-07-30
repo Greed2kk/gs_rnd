5bbab57d46d301d019537fe466141dfb1f1144a5
Greed2kk <draiver78@mail.ru>
2019-07-30 17:20:22 +0300

    add click coordinate and copy them to buffer

--- gs_rnd/client/static/views/map.js
+++ gs_rnd/client/static/views/map.js
@@ -8,10 +8,9 @@ import * as proj from 'ol/proj';
 import TileLayer from "ol/layer/Tile";
 import { fromLonLat } from "ol/proj";
 import XYZ from 'ol/source/XYZ';
-import {toLonLat} from 'ol/proj.js';
+import { toLonLat } from 'ol/proj.js';
 
 
-import {toStringHDMS} from 'ol/coordinate.js'
 import Overlay from 'ol/Overlay.js';
 
 import * as gas_def from '../icons/gas_def.png';
@@ -33,69 +32,62 @@ export class MapView extends MnView {
         });
         super(options);
     }
+    onAttach() {
+        //работа с попапами
+        let container = document.querySelector('#popup');
+        let content = document.querySelector('#popup-content');
+        let closer = document.querySelector('#popup-closer');
 
-    onAttach (){
+        let overlay = new Overlay({
+            element: container,
+            autoPan: true,
+            autoPanAnimation: {
+                duration: 250
+            }
+        });
+        closer.onclick = function () {
+            overlay.setPosition(undefined);
+            closer.blur();
+            return false;
+        };
+        //конец работы с попапами
+
         this.activeLayer = "OSM";
         this.map = new Map({
+            overlays: [overlay],
             target: 'main_view',
-             layers: [
-      new TileLayer({
-        source: new OSM()
-      })
-    ],
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                })
+            ],
             view: new View({
-                center: fromLonLat([ 39.719510, 47.232462]),
-                zoom: 13,
+                center: fromLonLat([39.72570419311523, 47.234897586694956]),
+                zoom: 14,
             })
         })
+        //попап на клик с координатами
+        this.map.on('singleclick', function (evt) {
+            let coordinate = evt.coordinate;
+            let hdms = toLonLat(coordinate);
 
+            //конец попапа на клик с координатами
 
-        this.map.on('singleclick', function(evt) {
-        let coordinate = evt.coordinate;
-        let hdms = toStringHDMS(toLonLat(coordinate));
+            //начало копирки
+            content.innerHTML = `Координаты клика: <div id='copy'>${hdms}</div>`;
+            overlay.setPosition(coordinate);
+               let button = document.getElementById('userButton');
+            let copy = document.getElementById('copy').textContent;
 
-        content.innerHTML = '<p>You clicked here:</p><code>' + hdms +
-            '</code>';
-        overlay.setPosition(coordinate);
-      });
-
-
+            button.addEventListener("click", function (event) {
+                event.preventDefault();
+                console.log(copy);
+                navigator.clipboard.writeText(copy);
+                document.execCommand("copy");
+            });
+            //конец копирки
+        });
 
     }
 
-//работа с попапами
-onLeftMapClick(){
-
-const container = document.getElementById('popup');
-const content = document.getElementById('popup-content');
-const closer = document.getElementById('popup-closer');
-
-var overlay = new Overlay({
-        element: container,
-        autoPan: true,
-        autoPanAnimation: {
-          duration: 250
-        }
-      });
-
-closer.onclick = function() {
-        overlay.setPosition(undefined);
-        closer.blur();
-        return false;
-      };
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
 };
-
