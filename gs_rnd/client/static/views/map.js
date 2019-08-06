efde21d59476e33e8141d647e8b23ac90473c38c
Greed2kk <draiver78@mail.ru>
2019-08-06 15:45:17 +0300

    FixBugWithFlyTo

--- gs_rnd/client/static/views/map.js
+++ gs_rnd/client/static/views/map.js
@@ -1,69 +1,44 @@
 import "ol/ol.css";
-import {
-    View as MnView
-} from 'backbone.marionette';
-import {
-    Map,
-    View
-} from 'ol';
-import OSM from 'ol/source/OSM';
-import * as proj from 'ol/proj';
+import { View as MnView } from "backbone.marionette";
+import { Map, View } from "ol";
+import OSM from "ol/source/OSM";
+import * as proj from "ol/proj";
 import TileLayer from "ol/layer/Tile";
-import {
-    fromLonLat
-} from "ol/proj";
-import XYZ from 'ol/source/XYZ';
-import {
-    toLonLat
-} from 'ol/proj.js';
-import {
-    defaults as defaultControls
-} from 'ol/control.js';
-import MousePosition from 'ol/control/MousePosition.js';
-import {
-    click
-} from 'ol/events/condition.js';
-import Select from 'ol/interaction/Select.js';
+import { fromLonLat } from "ol/proj";
+import XYZ from "ol/source/XYZ";
+import { toLonLat } from "ol/proj.js";
+import { defaults as defaultControls } from "ol/control.js";
+import MousePosition from "ol/control/MousePosition.js";
+import { click } from "ol/events/condition.js";
+import Select from "ol/interaction/Select.js";
 
-import Zoom from 'ol/control/Zoom';
-import {
-    easeIn,
-    easeOut
-} from 'ol/easing.js';
+import Zoom from "ol/control/Zoom";
+import { easeIn, easeOut } from "ol/easing.js";
 
 //для маркера
-import Point from 'ol/geom/Point.js';
-import Feature from 'ol/Feature.js';
-import {
-    Icon,
-    Style
-} from 'ol/style.js';
-import VectorSource from 'ol/source/Vector.js';
-import {
-    Vector as VectorLayer
-} from 'ol/layer.js';
+import Point from "ol/geom/Point.js";
+import Feature from "ol/Feature.js";
+import { Icon, Style } from "ol/style.js";
+import VectorSource from "ol/source/Vector.js";
+import { Vector as VectorLayer } from "ol/layer.js";
 
-import Overlay from 'ol/Overlay.js';
+import Overlay from "ol/Overlay.js";
 
 //import template_map from '../../templates/map_view.hbs';
-import main_view from '../../templates/main_view.hbs';
-import popup_view from '../../templates/popup_cords.hbs';
+import main_view from "../../templates/main_view.hbs";
+import popup_view from "../../templates/popup_cords.hbs";
 //import map_markers_view from '../../templates/map_markers_popup.hbs';
 
 export class MapView extends MnView {
-
     template() {
         return main_view(), popup_view();
     }
 
     ui() {
         return {
-            'popup': '#popup',
-            'content': '#popup-content',
-            'closer': '#popup-closer',
-            ////////////для списка//////////////
-            's-list': '.sights-list',
-            'cter': '.container',
+            popup: "#popup",
+            content: "#popup-content",
+            closer: "#popup-closer",
         };
     }
 
@@ -79,11 +54,11 @@ export class MapView extends MnView {
 
         overlay.setOffset([0, -20]);
 
-        this.ui.closer.on('click', () => {
+        this.ui.closer.on("click", () => {
             overlay.setPosition(undefined);
             this.ui.closer.blur();
             return false;
-        })
+        });
 
         function customLatLng(coord) {
             const [lat, lng] = coord;
@@ -92,16 +67,16 @@ export class MapView extends MnView {
 
         let mousePositionControl = new MousePosition({
             coordinateFormat: customLatLng,
-            projection: 'EPSG:4326',
-            className: 'custom-mouse-position',
-            undefinedHTML: '&nbsp;'
+            projection: "EPSG:4326",
+            className: "custom-mouse-position",
+            undefinedHTML: "&nbsp;"
         });
 
         this.activeLayer = "OSM";
         this.map = new Map({
             controls: defaultControls().extend([mousePositionControl]),
             overlays: [overlay],
-            target: 'main_view',
+            target: "main_view",
             layers: [
                 new TileLayer({
                     source: new OSM()
@@ -109,32 +84,28 @@ export class MapView extends MnView {
             ],
             view: new View({
                 center: fromLonLat([39.712277054786675, 47.23726874200372]),
-                zoom: 14,
+                zoom: 14
             })
         });
-        const {
-            collection
-        } = this.options;
+        const { collection } = this.options;
         const features = collection.map(item => {
-            const {
-                geometry,
-                name,
-                image
-            } = item.pick('geometry', 'name', 'image');
+            const { geometry, name, image } = item.pick("geometry", "name", "image");
             const feature = new Feature({
                 name,
-                geometry: new Point(fromLonLat(geometry)),
+                geometry: new Point(fromLonLat(geometry))
             });
-            feature.setStyle(new Style({
-                image: new Icon(({
-                    anchor: [0.5, 46],
-                    anchorXUnits: 'fraction',
-                    anchorYUnits: 'pixels',
-                    src: image,
-                    size: [64, 64],
-                    scale: 0.5,
-                }))
-            }));
+            feature.setStyle(
+                new Style({
+                    image: new Icon({
+                        anchor: [0.5, 46],
+                        anchorXUnits: "fraction",
+                        anchorYUnits: "pixels",
+                        src: image,
+                        size: [64, 64],
+                        scale: 0.5
+                    })
+                })
+            );
             return feature;
         });
 
@@ -144,19 +115,19 @@ export class MapView extends MnView {
         //console.log(vectorSource);
 
         let markerVectorLayer = new VectorLayer({
-            source: vectorSource,
+            source: vectorSource
         });
 
         this.map.addLayer(markerVectorLayer);
 
-        this.map.on('click', (e) => {
+        this.map.on("click", e => {
             this.map.forEachFeatureAtPixel(e.pixel, (feature, layer) => {
                 console.log(`Координаты ${feature.getGeometry().getCoordinates()}`);
                 const coordinate = feature.getGeometry().getCoordinates();
                 this.ui.content.html(`Это маркер: ${feature.values_.name}`);
                 overlay.setPosition(coordinate);
-            })
-        })
+            });
+        });
 
         this.initListeners();
     }
@@ -164,54 +135,58 @@ export class MapView extends MnView {
     initListeners() {
         const view = this.map.getView();
 
-        const model = this.getOption('collection').on('change:selected', (model, value, options) => {
-
-            if (value) {
-                function show() {
-                    const coords = model.get("geometry");
-                    return coords;
-                }
-                let coords = show();
-                console.log(coords);
-
-                function flyTo(location, done) {
-                    var duration = 2000;
-                    var zoom = view.getZoom();
-                    var parts = 2;
-                    var called = false;
-
-                    function callback(complete) {
-                        --parts;
-                        if (called) {
-                            return;
-                        }
-                        if (parts === 0 || !complete) {
-                            called = true;
-                            done(complete);
-                        }
+        const model = this.getOption("collection").on(
+            "change:selected",
+            (model, value, options) => {
+                if (value) {
+                    function show() {
+                        const coords = model.get("geometry");
+                        return fromLonLat(coords);
                     }
-                    view.animate({
-                        center: location,
-                        duration: duration
-                    }, callback);
-                    view.animate({
-                        zoom: zoom - 1,
-                        duration: duration / 2
-                    }, {
-                        zoom: zoom,
-                        duration: duration / 2
-                    }, callback);
+                    let coords = show();
+
+                    function flyTo(location, done) {
+                        let duration = 2000;
+                        let zoom = view.getZoom();
+                        let parts = 2;
+                        let called = false;
+
+                        function callback(complete) {
+                            --parts;
+                            if (called) {
+                                return;
+                            }
+                            if (parts === 0 || !complete) {
+                                called = true;
+                                done(complete);
+                            }
+                        }
+                        console.log(location);
+
+                        view.animate({
+                                center: location,
+                                duration: duration
+                            },
+                            callback
+                        );
+                        view.animate({
+                                zoom: zoom - 1,
+                                duration: duration / 2
+                            }, {
+                                zoom: zoom,
+                                duration: duration / 2
+                            },
+                            callback
+                        );
+                    }
+
+                    setTimeout(() => {
+                        flyTo(coords, function() {
+                            console.log("done");
+                        });
+                    }, 500);
                 }
-
-                setTimeout(() => {
-                    flyTo(coords, function() {
-                        console.log('done');
-                    });
-                }, 500)
-
             }
-
-        });
-
+        );
     }
-};
+}
