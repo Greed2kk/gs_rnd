bd76a4cc354191dea1169254a23035416d10c656
Greed2kk <draiver78@mail.ru>
2019-08-19 17:18:08 +0300

    admin

--- gs_rnd/admin.py
+++ gs_rnd/admin.py
@@ -1,3 +1,6 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import GasStation, Image
 
-# Register your models here.
+
+admin.site.register(GasStation)
+admin.site.register(Image)
