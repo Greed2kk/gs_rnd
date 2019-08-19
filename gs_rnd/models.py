f87eaf75fd2b7be490174ba3e666d859db05cef5
Greed2kk <draiver78@mail.ru>
2019-08-19 10:10:03 +0300

    add models

--- gs_rnd/models.py
+++ gs_rnd/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(verbose_name='Лого',)
+    logo = models.ImageField(verbose_name='Маркер',)
+
+
+class GasStation(models.Model):
+    title = models.CharField (max_length = 50, verbose_name='Название',)
+    coordinates = models.PointField (geography=True, verbose_name='Координаты',)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, verbose_name='Маркер',)
+    address = models.CharField(max_length = 100, verbose_name='Адрес')
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, verbose_name='Лого',)
