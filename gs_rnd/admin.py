f1a31a87d8cd889d5803a6ff656dec6cd0a2319e
Greed2kk <draiver78@mail.ru>
2019-08-20 14:45:40 +0300

    admin-add new GS

--- gs_rnd/admin.py
+++ gs_rnd/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
 from .models import GasStation, Image
 
 
-admin.site.register(GasStation)
+@admin.register(GasStation)
+class MarkerAdmin(OSMGeoAdmin):
+    default_lon = 4420582.70
+    default_lat = 5978522.25
+    default_zoom = 13
+
+
 admin.site.register(Image)
